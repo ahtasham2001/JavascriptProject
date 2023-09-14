273bb4b63149acd3a61a503a4055eb71d1156e73
AHTASHAM NAEEM <141627116+ahtasham2001@users.noreply.github.com>
2023-09-14 11:07:13 +0000

    app js update

--- 5.passwordGenerator/app.js
+++ 5.passwordGenerator/app.js
@@ -12,6 +12,8 @@ const indicator = document.querySelector("[data-Indicator]");
 const generateBtn = document.querySelector(".generate-button");
 const allCheckBox = document.querySelector("input[type=checkbox]");
 
+let symbols = '~!@#$%^&*()_+[{]}|\'";:.<,>?/'
+
 
 let password = "";
 let passwordLenght = 15;
@@ -31,6 +33,40 @@ function setIndicator(){
     // shadow
 }
 
-function randInt(){
+function getRndInteger(){
     return Math.floor(Math.random()*(max-min)+min);
+}
+function generateRandomNumber(){
+    return getRndInteger(0,9);
+}
+function generateLowerCase(){
+    return String.fromCharCode(getRndInteger(97,123));
+}
+function generateUpperCase(){
+    return String.fromCharCode(getRndInteger(65,91));
+}
+function generateSymbol(){
+    const randNum = getRndInteger(0, symbols.length);
+    return symbols.charAt(randNum);
+}
+
+function calcStrenght(){
+    let hasUpper = false;
+    let hasLower = false;
+    let hasNum = false;
+    let hasSym = false;
+
+    if(uppercaseCheck.checked) hasUpper = true;
+    if(lowercaseCheck.checked) hasLower = true;
+    if(numberscaseCheck.checked) hasNum = true;
+    if(symbolscaseCheck.checked) hasSym = true;
+
+    if(hasUpper && hasLower && (hasNum || hasSym)&& passwordLenght){
+        setIndicator("#0f0");
+    }else if((hasLower || hasUpper)&&(hasNum || hasSym)&& passwordLenght>=6){
+        setIndicator("#ff0");
+    }else{
+        setIndicator("#f00")
+    }
+
 }
