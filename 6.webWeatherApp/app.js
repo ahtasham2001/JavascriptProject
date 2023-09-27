9f4e162c985c0b8b75bac2ca4ecfd47e9e30c072
AHTASHAM NAEEM <141627116+ahtasham2001@users.noreply.github.com>
2023-09-27 11:17:34 +0000

    JS START

--- 6.webWeatherApp/app.js
+++ 6.webWeatherApp/app.js
@@ -1,2 +1,2 @@
-document.querySelector("[data-userWeather]")
-document.querySelector("[data-searchWeather]")
+const userTab = document.querySelector("[data-userWeather]")
+const searchTab = document.querySelector("[data-searchWeather]")
